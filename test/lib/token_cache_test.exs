ef03e6582954f9d6cf260289dc36d88f259a4618
lessless <me@lessless.pp.ua>
2015-10-02 21:51:30 +0700

    remove storage key

--- test/lib/token_cache_test.exs
+++ test/lib/token_cache_test.exs
@@ -5,20 +5,31 @@ defmodule GCloudStorage.TokenCacheTest do
   alias GCloudStorage.AccessToken
   alias GCloudStorage.TestConfig, as: Config
 
-  setup do
+  setup_all do
    {:ok, _pid} = TokenCache.start_link
    :ets.new(Config.ets[:table], [:set, :public, :named_table, {:read_concurrency, true}])
+    on_exit fn ->
+      if (:ets.info(Config.ets[:table]) != :undefined) do
+        :ets.delete_all_objects(Config.ets[:table])
+      end
+      :ok
+    end
    :ok
   end
 
 
   test "it refresh  expired token" do
     secret = "secret"
+    timeout = div(Config.token[:timeout], 2)
+    token = TokenCache.access_token
 
-    assert TokenCache.access_token != secret
-    :timer.sleep(Config.token[:timeout])
+    refute token  == secret
+    :timer.sleep(timeout)
+
+    assert token == TokenCache.access_token
+    :timer.sleep(timeout)
+
     :ets.insert(Config.ets[:table], {Config.ets[:token_key], secret})
     assert TokenCache.access_token == secret
-    :ets.delete_all_objects(Config.ets[:table])
   end
 end
