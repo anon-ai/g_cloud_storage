69632c55daf77fd588b8e639fea36dd1442f4567
lessless <me@lessless.pp.ua>
2015-10-08 22:17:15 +0700

    add per-environment configuration

---
+++ config/test.exs
@@ -0,0 +1,7 @@
+use Mix.Config
+
+config :g_cloud_storage,
+  credentials:    "test/fixture/storage.json",
+  private_key:    "test/fixture/storage.key.pem",
+  token_provider: GCloudStorage.DummyAccessToken,
+  token_lifetime: 2
