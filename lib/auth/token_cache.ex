e090af6e535b09164f1c767d7bf01413a94744cb
Yevhenii Kurtov <lessless@users.noreply.github.com>
2016-01-11 08:12:27 +0700

    Re-align comments

--- lib/auth/token_cache.ex
+++ lib/auth/token_cache.ex
@@ -1,7 +1,7 @@
 defmodule GCloudStorage.TokenCache do
   use GenServer
+  
   #Client API
-
   def start_link do
     GenServer.start_link(__MODULE__, {%GCloudStorage.AccessToken{}, {}}, name: __MODULE__)
   end
@@ -11,7 +11,6 @@ defmodule GCloudStorage.TokenCache do
   end
 
   # Server API
-
   def handle_call(:access_token, _from, {%GCloudStorage.AccessToken{token: ""}, _}) do
     refresh
   end
